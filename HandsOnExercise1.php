f852cbd65368b2604c1b44e179f177721ae1e747
Camilo Joshua M. Pascua <72638150+Cjpascua@users.noreply.github.com>
2022-10-06 19:59:57 +0800

    Add files via upload

---
+++ HandsOnExercise1.php
@@ -0,0 +1,57 @@
+<title>Hands On Exercise 1 </title>
+<h2>PHP exercise 1</h2>
+<?php
+echo "Twinkle, Twinkle little star.";
+$twinkle = "Twinkle";
+$star = "star";
+?>
+
+<h2>PHP exercise 2</h2>
+<?php
+$x = 10;
+$y = 7;
+
+echo $x + $y;
+echo("<br>");
+echo $x - $y;
+echo("<br>");
+echo $x * $y;
+echo("<br>");
+echo $x / $y;
+echo("<br>");
+echo $x % $y;
+echo("<br>");
+?>
+
+<h2>PHP exercise 3</h2>
+<?php
+$variable = 8;
+echo "Value is now $variable \r";
+echo("<br>");
+echo "Add 2. Value is now ". ($variable += 2);
+echo("<br>");
+echo "Subtract 4. Value is now ". ($variable -= 4);
+echo("<br>");
+echo "Multiply by 5. Value is now ". ($variable *= 5);
+echo ("<br>");
+echo "Divide by 3. Value is now ". ($variable /= 3);
+echo ("<br>");
+echo "Increment value by one. Value is now ". ($variable++);
+echo ("<br>");
+echo "Decrement value by one. Value is now ". ($variable--);
+
+?>
+
+<h2>PHP exercise 4</h2>
+<?php
+$name = "Harry";
+var_dump("Harry");
+echo "<br>";
+echo  $name;
+echo "<br>";
+var_dump(28);
+echo "<br>";
+var_dump($name = null);
+?>
+
+
